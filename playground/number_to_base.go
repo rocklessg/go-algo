d383e220a8193164619bdb98342cd6ee432c0626
rocklessg <abdulhafiz.suleiman@decagon.dev>
2025-08-18 15:10:54 +0100

    New logic implementation

--- playground/number_to_base.go
+++ playground/number_to_base.go
@@ -2,8 +2,9 @@ package playground
 
 import (
 	//"strconv"
-	"strings")
-
+	"fmt"
+	"strings"
+)
 
 // func NumberToBase(number int, base int) string {
 // 	var remainder int = number
@@ -50,4 +51,18 @@ func NumberToBase(number int, base int) string {
 
 	// Reverse the result string
 	return ReverseString(result.String())
+}
+
+
+// Another logic
+func NumberToBaseNew(value string, base int) int {
+	res := 0
+	multiplier := 1
+	for i := len(value) - 1; i >= 0; i-- {
+		var val int
+		fmt.Scanf(string(value[i]), "%X", &val)
+		res += multiplier * val
+		multiplier *= base
+	}
+	return res
 }
